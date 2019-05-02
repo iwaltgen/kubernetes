296a08c67f49255f81394f1972cec05f7fde0c73
iwaltgen <iwaltgen@gmail.com>
2019-05-02 19:18:33 +0900

    fix: swap on mode 설명 추가

--- kubeadm_init.sh
+++ kubeadm_init.sh
@@ -2,6 +2,12 @@
 
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16 | sudo tee kubeadm_init_output
 
+## kubernetes swap on mode ##
+# sed -i '9s/^/Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"\n/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+# systemctl daemon-reload
+# systemctl restart kubelet
+# sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors Swap | sudo tee kubeadm_init_output
+
 mkdir -p $HOME/.kube
 sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -9,5 +15,6 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
+## kubernetes single node mode ##
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
