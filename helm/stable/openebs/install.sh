211ff4bc7fb3d746893986dcae2a8d179109285c
iwaltgen <iwaltgen@gmail.com>
2019-07-20 00:50:05 +0900

    fix: openebs set default storageclass

--- helm/stable/openebs/install.sh
+++ helm/stable/openebs/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 helm install stable/openebs --name openebs --namespace openebs -f values.yaml
+
+kubectl patch sc openebs-cstor-sparse -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
